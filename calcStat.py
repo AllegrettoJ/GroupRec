4703ab66f7d87f2dbae552840a5e4d43a69b19d7
AllegrettoJ <31532396+AllegrettoJ@users.noreply.github.com>
2019-09-30 03:41:19 +1000

    Update calcStat.py

--- calcStat.py
+++ calcStat.py
@@ -1,9 +1,14 @@
 import pandas as pd
+from scipy import spatial
+from scipy.spatial.distance import cosine
+import statistics
 
 def calMean(budget,startNode):
-    results = pd.read_csv('resultstest.csv', sep="\t")
+    results = pd.read_csv('resultstest.csv')
     print(results.to_string())
-    resultsMean = results.groupby('cluster')['profit'].mean().reset_index(name='avgProfit')
+    resultsMean = results.groupby('cluster')['totalPopInt'].sum().reset_index(name='totalprofit')
+    print(resultsMean)
+    #resultsMean = results.groupby('cluster')['profit'].mean().reset_index(name='avgProfit')
     resultsMean['budget'] = budget
     resultsMean['startNode'] = startNode
     '''
@@ -14,5 +19,30 @@ def calMean(budget,startNode):
     '''
     print(resultsMean)
     return resultsMean
+
+def calcIntCosSim(userInGroup, dfInterest, binaryInt):
+    cosVec =[]
+    results = []
+    userIntByTime = pd.DataFrame(columns=dfInterest.columns)
+    for i in range(len(userInGroup)):
+        userIntByTime = userIntByTime.append(dfInterest.loc[dfInterest['userID'] == userInGroup[i]])
+    userIntByTime = userIntByTime.drop(['userID'], axis=1)
+    print(userIntByTime)
+    userIntByTime = userIntByTime.reset_index(drop=True)
+    if binaryInt == True:
+        userIntByTime[userIntByTime != 0] = 1
+    print(userIntByTime)
+    for i in range(len(userIntByTime.index)):
+        for j in range(len(userIntByTime.index)):
+            if i != j:
+                cosTem = 1 - cosine(userIntByTime.iloc[i], userIntByTime.iloc[j])
+                cosVec.append(cosTem)
+
+    print(len(cosVec))
+    print(cosVec)
+    cos = statistics.mean(cosVec)
+    print(statistics.mean(cosVec))
+    return cos
+
 
 #calMean(budget = 300,startNode=7)
